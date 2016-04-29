1f42f2fe345e377b1fdf84bbc48cb5c980a49a16
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-04-29 15:52:50 -0300

    Agregando validador de formulario y configuración para emitir el mail desde sendgrid.

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+var apiKey = process.env.MAIL_API_KEY;
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/', express.static("public"));
 
-app.listen(5000, function() { console.log('listening on port 5000')});
+app.post('/sendMail', function(req, res) {
+  var sendgrid = require("sendgrid")(apiKey);
+  var email = new sendgrid.Email();
+
+  email.addTo("hugoruscitti@gmail.com");
+  email.setFrom("hugoruscitti@gmail.com");
+
+  email.setSubject("CONTACTO WEB: " + req.body.subject);
+  email.setHtml("Sender: " + req.body.email + "<p>" +
+                "message: " + "<p><pre>" + req.body.message + "</pre>");
+
+  sendgrid.send(email);
+
+  console.log(req.body);
+  res.end("Gracias!");
+});
+
+app.listen(5000, function() {
+  console.log('listening on port 5000');
+});
